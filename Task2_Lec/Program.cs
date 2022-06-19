6b53e1cc6c85532a64a66401849cf17a4b1052ef
Sergey <serega79ser@mail.ru>
2022-06-19 14:56:18 +0600

    Dif

---
+++ Task2_Lec/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число");	
+double numberA = Convert.ToDouble(Console.ReadLine());;
+
+Console.WriteLine("Введите второе число");
+double numberB = Convert.ToDouble(Console.ReadLine());;
+
+
+Console.WriteLine($" Произведение чисел {numberA} / {numberB} = {numberA/numberB}");

8966ddd86755ec1e992651b96a7d6466e779a6b1
Sergey <serega79ser@mail.ru>
2022-06-19 14:40:49 +0600

    Sum

---
+++ Task1_Sum/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+int numberA = new Random().Next(1, 10); // 1 2 3 4 .. . 9
+Console.WriteLine("Введите первое число");
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine("Введите второе число");
+Console.WriteLine(numberB);
+Console.WriteLine($"Сумма {numberA} + {numberB} = {numberA + numberB}");

65c2cd5624d5f9ca082d8cf3ca99f58ac1681e24
Sergey <serega79ser@mail.ru>
2022-06-19 14:50:36 +0600

    Sum

--- Task1_Sum/Program.cs
+++ Task1_Sum/Program.cs
@@ -1,8 +1,10 @@
 ﻿Console.Clear();
-int numberA = new Random().Next(1, 10); // 1 2 3 4 .. . 9
-Console.WriteLine("Введите первое число");
-Console.WriteLine(numberA);
-int numberB = new Random().Next(1, 10);
-Console.WriteLine("Введите второе число");
-Console.WriteLine(numberB);
+//int numberA = new Random().Next(1, 10); // 1 2 3 4 .. . 9
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(numberA);
+//int numberB = new Random().Next(1, 10);
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(numberB);
 Console.WriteLine($"Сумма {numberA} + {numberB} = {numberA + numberB}");

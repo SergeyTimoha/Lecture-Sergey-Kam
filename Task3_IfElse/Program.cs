c6a814869039ac68cb193efaee67d90d7eaaee1b
Sergey <serega79ser@mail.ru>
2022-06-19 23:37:46 +0600

    If Else

---
+++ Task3_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string uzername = Console.ReadLine();
+
+if(uzername.ToLower() == "маша")
+{
+    Console.WriteLine("Ура это же Маша");
+}
+    
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(uzername);
+}
